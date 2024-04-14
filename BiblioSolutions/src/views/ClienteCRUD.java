aaa321e56d2badf711acc36490059d3a559b7c1b
Mikael Burgos <mikaelburgos1997@gmail.com>
2024-04-14 09:45:25 -0400

    Fixed ClienteCRUD and added logic

--- BiblioSolutions/src/views/ClienteCRUD.java
+++ BiblioSolutions/src/views/ClienteCRUD.java
@@ -94,12 +94,7 @@ public class ClienteCRUD extends javax.swing.JFrame {
         int resultado = clienteDAO.agregar(cliente);
         if (resultado == 1) {
             listarClientes();
-            // Limpiar los campos de texto después de agregar un cliente
-            jTextField1.setText("");
-            jTextField2.setText("");
-            jTextField3.setText("");
-            jTextField4.setText("");
-            jTextField5.setText("");
+            
         }
     }
       
@@ -144,6 +139,8 @@ public class ClienteCRUD extends javax.swing.JFrame {
             }
         }
     }
+       
+       
 
 
     /**
